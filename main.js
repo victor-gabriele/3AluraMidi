4a169357c5a0a07243b14b1bb5277c51781b0287
victor-gabriele <134950739+victor-gabriele@users.noreply.github.com>
2023-10-09 12:15:08 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,47 +1,16 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
-    document.querySelector('.tecla_pom').onclick = tocaSomPom;
-}
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
-    document.querySelector('.tecla_clap').onclick = tocaSomClap;
-}
-function tocaSomTim(){
-    document.querySelector('#som_tecla_tim').play();
-    document.querySelector('.tecla_tim').onclick = tocaSomTim;
-}
-function tocaSomPuff(){
-    document.querySelector('#som_tecla_puff').play();
-    document.querySelector('.tecla_puff').onclick = tocaSomPuff;
-}
-function tocaSomSplash(){
-    document.querySelector('#som_tecla_splash').play();
-    document.querySelector('.tecla_splash').onclick = tocaSomSplash;
-}
-function tocaSomToim(){
-    document.querySelector('#som_tecla_toim').play();
-    document.querySelector('.tecla_toim').onclick = tocaSomToim;
-}
-function tocaSomPsh(){
-    document.querySelector('#som_tecla_psh').play();
-    document.querySelector('.tecla_psh').onclick = tocaSomPsh;
-}
-function tocaSomTic(){
-    document.querySelector('#som_tecla_tic').play();
-    document.querySelector('.tecla_tic').onclick = tocaSomTic;
-}
-function tocaSomTom(){
-    document.querySelector('#som_tecla_tom').play();
-    document.querySelector('.tecla_tom').onclick = tocaSomTom;
-}
-document.querySelectorAll('.tecla');
-const listaDeTeclas = document.querySelectorAll('.tecla');
-listaDeTeclas[contador].onclick = tocaSomPom;
-let contador = 0;
+function tocaSom(idElementoAudio){
+     document.querySelector(idElementoAudio).play();}
 
-//Estrutura de repetição-Enquanto
-while(contador < listaDeTeclas.length) {
 
-contador = contador + 1;
+    const listaDeTeclas=document.querySelectorAll('.tecla');
 
-}
+    let contador = 0;
+    //Estrura de repetição - Enquanto
+    while (contador <listaDeTeclas.length){
+        listaDeTeclas[contador].onclick = function(){
+            tocaSom('#som_tecla_splash');
+        };
+        contador = contador + 1 ;
+    }
+
+

d91d95ff2ba4eb04aa73b8a08fa05b972e93d176
victor-gabriele <134950739+victor-gabriele@users.noreply.github.com>
2023-10-16 12:25:16 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 function tocaSom(idElementoAudio){
      document.querySelector(idElementoAudio).play();}
 
-
-    const listaDeTeclas=document.querySelectorAll('.tecla');
+const listaDeTeclas=document.querySelectorAll('.tecla');
 
     let contador = 0;
     //Estrura de repetição - Enquanto
     while (contador <listaDeTeclas.length){
+        const tecla = listaDeTeclas[contador];
+        const instrumento = tecla.classList[1];
+        const idAudio = `#som_ ${instrumento}`;
+        console.log(instrumento);
         listaDeTeclas[contador].onclick = function(){
-            tocaSom('#som_tecla_splash');
+            tocaSom(idAudio);
         };
         contador = contador + 1 ;
-    }
+        console.log(contador);
+        console.log(idAudio);
+ }
+ 
 
 
